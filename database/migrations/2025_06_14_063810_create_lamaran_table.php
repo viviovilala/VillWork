b7925340d4bf90ba954f7de258c2e09fe9acfd4b
malhandha <malfianhandiy@gmail.com>
2025-06-14 14:03:34 +0700

    update db

---
+++ database/migrations/2025_06_14_063810_create_lamaran_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lamaran', function (Blueprint $table) {
+            $table->id();
+            $table->string('linkedin')->nullable();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('lowongan_id')->constrained('lowongan')->cascadeOnDelete();
+            $table->enum('status', ['Diproses', 'Diterima', 'Ditolak'])->default('Diproses');
+            $table->text('pesan');
+            $table->string('cv_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lamaran');
+    }
+};
