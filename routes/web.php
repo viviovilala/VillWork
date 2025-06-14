cdac78923ab64f000a81ee3ab13219715d980099
malhandha <malfianhandiy@gmail.com>
2025-06-14 19:58:28 +0700

    revisi folder

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/lamaran', function(){
+    return view('lamaran');
+})->middleware(['auth', 'verified'])->name('lamaran');
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
